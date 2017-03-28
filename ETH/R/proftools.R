3b33d306b0a36245a380888d634d84a1bab593b8
Martin Maechler <maechler@r-project.org>
2017-03-28 11:51:22 +0200

    new (for week 6)

---
+++ ETH/R/proftools.R
@@ -0,0 +1,16 @@
+###  Reading and *EXPLORING*  the vignette of package  'proftools':
+
+## Install the package if we don't have it:
+if(!require("proftools"))  install.packages("proftools")
+
+packageDescription("proftools")## ok, by  __Luke Tierney__ an R Core guru
+
+## We explore the installed package  _manually_ here
+## The ./doc/  sub-directory (= sub-folder) has vignettes (and potentially more):
+(doc.dir <- system.file(package="proftools", "doc"))
+
+list.files(doc.dir)
+                 ## vvvvvvvvvvvvvvv to get full path names:
+list.files(doc.dir, full.names=TRUE)
+
+## --> now open proftools.R
