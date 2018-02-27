96e57dedb8e435b9f9670d325f8f2bab96ca13c5
Martin Maechler <maechler@r-project.org>
2018-02-27 17:42:58 +0100

    update from class today

--- ETH/R/functions-1.R
+++ ETH/R/functions-1.R
@@ -8,12 +8,15 @@
 c
 ## Help page:
 ?c #===>  Read the "hierarchy" part in Details
+`<-`
 `[`
 
 ## What *function* is  `[` ?
 x <- 10:20
+`:`
 x[2]   # is exactly the same as
 `[`(x, 2)
+`[`(x, 2:3)
 
 x  # everything that happens is a function call -- which function here ?
 
@@ -30,6 +33,7 @@ methods("print") # *many*   --- do also have a look the help page  ?methods
 class(`[`)
 class("[")
 typeof(`[`)
+typeof(`c`)
 typeof(ls)
 `[<-`(x,2,13) # is almost the same as
 x[2] <- 13    # << (here `x` is changed)
@@ -37,14 +41,16 @@ methods("[<-")
 
 ## The difference between "[" and "[[" already shows with a simple named vector:
 x <- 1:10
-names(x)  <- LETTERS[1:10]
+names(x)  <- LETTERS[1:10]# does *not* call 'names' function
+x
+Q <- quantile(rnorm(50))
 sum(x) # it is a vector
 x["C"]
 x[["C"]]
 `names<-`(x,letters[1:10] )
 x <- `names<-`(x,letters[1:10] )
 
-[## Atomic objects:  numeric, character, logical,... vectors
+## Atomic objects:  numeric, character, logical,... vectors
 ## ---------------  and even matrices of those:
 is.atomic(diag(4))
 
@@ -53,6 +59,11 @@ D
 dim(D) <- NULL   # ... a function call:  which function is called here?
 D
 `dim<-`(D, c(4,4))
-D  <- diag(4); D # matrix ... but can also be treated as a vector:
+D  <- diag(1:4); D # matrix ... but can also be treated as a vector:
 D[7:9]
 D[11]
+
+`names<-`
+typeof(`names<-`) # builtin
+
+`dim<-`
