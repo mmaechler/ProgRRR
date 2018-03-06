ba49c3b8dfc54579f3307c5a7d5bcd95885f025e
Martin Maechler <maechler@r-project.org>
2018-03-06 16:20:33 +0100

    Choose in which environment to evaluate -- prompted by Francesco's question

---
+++ ETH/R/envex2.R
@@ -0,0 +1,31 @@
+s <- function() a+b
+s() # error
+
+e1 <- new.env()
+e1$a <- 1:3
+e1$b <- 1000
+
+e2 <- new.env()
+e2$a <- 10:20
+e2$b <- 100^2
+
+eval(expression( a+b ), envir = e1)# ok
+eval(expression( a+b ), envir = e2)# ok
+eval(     quote( a+b ), envir = e1)# ok
+eval(quote( s() ), envir = e1)# error
+environment(s)
+
+## to make it work:
+environment(s) <- e1
+s()
+environment(s) <- e2
+s()
+
+### Let's assume an auxiliary function in package
+
+## 1) print it to console
+## 2) copy&paste to *.R  script
+## 3) "fix the bug"
+## 4) "send to console" (evaluate in globalenv)
+## 5) This is not sufficient
+environment(<thisFun>) <- asNamespace("pkgName")
